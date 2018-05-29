7550eea5dad48d4d4692f014e0545144231a5ef4
sachindk1625 <sachindk1625@gmail.com>
2018-05-29 01:21:51 -0500

    auth-guard

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { LoginComponent } from './login/login.component';
 import {RouterModule} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -42,17 +43,20 @@ import { AuthService } from './auth.service';
     RouterModule.forRoot([
       {path:'', component:HomeComponent},
       {path:'products',component:ProductsComponent},
-      {path:'my/orders',component:MyOrdersComponent},
       {path:'shopping-cart',component:ShoppingCartComponent},
-      {path:'check-out',component:CheckOutComponent},
-      {path:'order-success',component:OrderSuccessComponent},
       {path:'login',component:LoginComponent},
-      {path:'admin/products',component:AdminProductsComponent},
-      {path:'admin/orders',component:AdminOrdersComponent}
+      
+      {path:'my/orders',component:MyOrdersComponent, canActivate:[AuthGuard] },      
+      {path:'check-out',component:CheckOutComponent, canActivate:[AuthGuard]},
+      {path:'order-success',component:OrderSuccessComponent, canActivate:[AuthGuard]},
+     
+      {path:'admin/products',component:AdminProductsComponent, canActivate:[AuthGuard]},
+      {path:'admin/orders',component:AdminOrdersComponent, canActivate:[AuthGuard]}
     ])
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
