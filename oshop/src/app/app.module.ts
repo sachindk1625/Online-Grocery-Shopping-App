3140d3b8a3eb08961d7c116ffd7d21d8ffbb5cec
sachindk1625 <sachindk1625@gmail.com>
2018-06-04 23:55:12 -0500

    product catalog complete

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -27,7 +27,9 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { CategoryService } from './category.service';
 import {FormsModule} from '@angular/forms';
 import { ProductService } from './product.service';
-import { CustomFormsModule } from 'ng2-validation'
+import { CustomFormsModule } from 'ng2-validation';
+import { ProductFilterComponent } from './products/product-filter/product-filter.component';
+import { ProductCardComponent } from './product-card/product-card.component'
 
 @NgModule({
   declarations: [
@@ -42,7 +44,9 @@ import { CustomFormsModule } from 'ng2-validation'
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
@@ -54,7 +58,7 @@ import { CustomFormsModule } from 'ng2-validation'
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      {path:'', component:HomeComponent},
+      {path:'', component:ProductsComponent},
       {path:'products',component:ProductsComponent},
       {path:'shopping-cart',component:ShoppingCartComponent},
       {path:'login',component:LoginComponent},
--- oshop/src/app/products/products.component.ts
+++ oshop/src/app/products/products.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { query } from '@angular/core/src/animation/dsl';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
 
-  constructor() { }
+  products:Product[]=[];
+  filteredProducts:Product[]=[];
+  category:string;
+ 
 
-  ngOnInit() {
-  }
+  constructor(
+    route:ActivatedRoute,
+    private productService:ProductService) {
+    this.productService.getAll().subscribe(products=>this.filteredProducts=this.products=products);
+   
+
+    route.queryParamMap.subscribe(prams=>{
+      this.category=prams.get('category');
+
+     this.filteredProducts= (this.category)? 
+     this.products.filter(p=>p.category===this.category ):this.products;
+
+    });
+
+    
+
+   }
+
+  
 
 }
